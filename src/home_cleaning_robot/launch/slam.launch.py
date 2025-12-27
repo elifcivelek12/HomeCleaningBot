7421c081e01977da4f4616cce3f5070990d03bbb
elifcivelek12 <elifceydacivelek@gmail.com>
2025-12-27 18:24:55 +0300

    syntax hatası düzeltildi

--- src/home_cleaning_robot/launch/slam.launch.py
+++ src/home_cleaning_robot/launch/slam.launch.py
@@ -3,7 +3,7 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
-def generate_launch_description()
+def generate_launch_description():
     pkg_dir = get_package_share_directory('home_cleaning_robot')
     
     # Ayar dosyasının yolu
